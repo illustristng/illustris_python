2a83f594a642868af92f53375bdc69fcd2be1397
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2015-02-25 19:26:37 -0500

    Initial commit (including basic group catalog access).

---
+++ README.md
@@ -0,0 +1,8 @@
+# README #
+
+The Illustris Simulation: Public Data Release
+
+Example code (Python).
+
+See the [Illustris Website Data Access Page](http://www.illustris-project.org/data/) for details.
+
---
+++ groupcat.py
@@ -0,0 +1,104 @@
+""" Illustris Simulation: Public Data Release.
+groupcat.py: File I/O related to the FoF and Subfind group catalogs. """
+
+import numpy as np
+import h5py
+import pdb
+
+def gcPath(basePath,snapNum,chunkNum=0):
+    """ Return absolute path to a group catalog HDF5 file (modify as needed). """
+    gcPath = basePath + '/groups_' + str(snapNum).zfill(3) + '/'
+    filePath = gcPath + 'fof_subhalo_tab_' + str(snapNum).zfill(3)
+    filePath += '.' + str(chunkNum) + '.hdf5'
+    
+    return filePath
+
+def loadObjects(basePath,snapNum,gName,nName,fields):
+    """ Load either halo or subhalo information from the group catalog. """
+    result = {}
+    
+    # load header from first chunk
+    with h5py.File(gcPath(basePath,snapNum),'r') as f:
+
+        header = dict( f['Header'].attrs.items() )
+        result['count'] = f['Header'].attrs['N'+nName+'_Total']
+        
+        # if fields not specified, load everything
+        if not fields:
+            fields = f[gName].keys()
+        
+        for field in fields:
+            # replace local length with global
+            shape = list(f[gName][field].shape)
+            shape[0] = result['count']
+        
+            # allocate within return dict
+            result[field] = np.zeros( shape, dtype=f[gName][field].dtype )
+        
+    # loop over chunks
+    wOffset = 0
+    
+    for i in range(header['NumFiles']):
+        filePath = gcPath(basePath,snapNum,i)
+        f = h5py.File(filePath,'r')
+        
+        # loop over each requested field for this particle type
+        for field in fields:
+            # shape and type
+            shape = f[gName][field].shape
+ 
+            # read data local to the current file (allocate dataset if it does not already exist)
+            if len(shape) == 1:
+                result[field][wOffset:wOffset+shape[0]] = f[gName][field][0:shape[0]]
+            else:
+                result[field][wOffset:wOffset+shape[0],:] = f[gName][field][0:shape[0],:]
+        
+
+        wOffset += shape[0]
+        f.close()
+        
+    return result
+        
+def loadSubhalos(basePath,snapNum,fields=None):
+    """ Load all subhalo information from the entire group catalog for one snapshot
+       (optionally restrict to a subset given by fields). """
+    
+    return loadObjects(basePath,snapNum,"Subhalo","subgroups",fields)
+
+def loadHalos(basePath,snapNum,fields=None):
+    """ Load all halo information from the entire group catalog for one snapshot
+       (optionally restrict to a subset given by fields). """
+    
+    return loadObjects(basePath,snapNum,"Group","groups",fields)
+    
+def loadSingle(fileBase,snapNum,haloID=None,subhaloID=None):
+    """ Return complete subhalo (subfind) information on one group. 
+        Search for subhalo/subgroup if getGroup==0, else search for halo/fof group. """
+
+    if (not haloID and not subhaloID) or (haloID and subhaloID):
+        raise Exception("Must specify either haloID or subhaloID (and not both).")
+        
+    dsetName = "Subhalo" if subhaloID else "Group"
+ 
+    # load groupcat offsets, calculate target file and offset
+    filePath = fileBase + '/postprocessing/offsets/offsets_'+dsetName.lower()+'_'+str(snapNum)+'.npy'
+    offsets = np.load(filePath)
+ 
+    offsets = searchID - offsets
+    fileNum = np.max( np.where(offsets >= 0) )
+    groupOffset = offsets[fileNum]
+ 
+    # load subhalo fields into a dict
+    filePath = fileBase + '/output/groups_' + str(snapNum).zfill(3) + '/'
+    filePath += 'fof_subhalo_tab_' + str(snapNum).zfill(3) + '.' + str(fileNum) + '.hdf5'
+ 
+    result = {}
+ 
+    f = h5py.File(filePath,'r')
+ 
+    for haloProp in f[dsetName].keys():
+        result[haloProp] = f[dsetName][haloProp][groupOffset]
+ 
+    f.close()
+    return result
+    
