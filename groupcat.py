f36e5fd4d29ea3c380b1f8abef41a32191fd5e01
Dylan Nelson <dnelson@cfa.harvard.edu>
2017-11-01 13:51:50 -0400

    Protect against basePath's without trailing slash.

--- groupcat.py
+++ groupcat.py
@@ -21,7 +21,7 @@ def gcPath(basePath, snapNum, chunkNum=0):
 
 def offsetPath(basePath, snapNum):
     """ Return absolute path to a separate offset file (modify as needed). """
-    offsetPath = basePath + '../postprocessing/offsets/offsets_%03d.hdf5' % snapNum
+    offsetPath = basePath + '/../postprocessing/offsets/offsets_%03d.hdf5' % snapNum
 
     return offsetPath
 
