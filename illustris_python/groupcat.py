3a34579d0524bffc60615d9e4874d18bcfa9f49a
Dylan Nelson <dnelson@uni-heidelberg.de>
2026-01-29 17:51:08 +0100

    Update groupcat.loadObjects() with multiprocessing-based parallel reader (set nThreads argument, or OMP_NUM_THREADS>1, to enable).

--- illustris_python/groupcat.py
+++ illustris_python/groupcat.py
@@ -3,10 +3,14 @@ groupcat.py: File I/O related to the FoF and Subfind group catalogs. """
 from __future__ import print_function
 
 import six
+from os import environ
 from os.path import isfile,expanduser,join
 import numpy as np
 import h5py
 from pathlib import Path
+
+import multiprocessing as mp
+from functools import partial
 
 
 def gcPath(basePath, snapNum, chunkNum=0):
@@ -27,9 +31,29 @@ def offsetPath(basePath, snapNum):
     return offsetPath
 
 
-def loadObjects(basePath, snapNum, gName, nName, fields):
+def _readfunc(basePath, snapNum, gName, nName, fields, i):
+    """ Multiprocessing target for loadObjects() below. """
+    result = {}
+
+    f = h5py.File(gcPath(basePath, snapNum, i), 'r')
+
+    if not f['Header'].attrs['N'+nName+'_ThisFile']:
+        f.close()
+        return None # empty file chunk
+
+    # loop over each requested field and read data local to this chunk
+    for field in fields:
+        result[field] = f[gName][field][()]
+
+    f.close()
+    return result
+
+def loadObjects(basePath, snapNum, gName, nName, fields, nThreads=None):
     """ Load either halo or subhalo information from the group catalog. """
     result = {}
+
+    if nThreads is None:
+        nThreads = environ.get('OMP_NUM_THREADS', 1)
 
     # make sure fields is not a single element
     if isinstance(fields, six.string_types):
@@ -49,6 +73,23 @@ def loadObjects(basePath, snapNum, gName, nName, fields):
             print('warning: zero groups, empty return (snap=' + str(snapNum) + ').')
             return result
 
+    # special case: single file? about x2 faster because of overhead of ndarray[:] = data, rather than ndarray = data.
+    if header['NumFiles'] == 1:
+        with h5py.File(gcPath(basePath, snapNum), 'r') as f:
+            for field in fields:
+                result[field] = f[gName][field][()]
+        if len(fields) == 1:
+            return result[fields[0]]
+        return result
+
+    with h5py.File(gcPath(basePath, snapNum), 'r') as f:
+        # find a chunk with objects of this type
+        i = 1
+        while len(f[gName].keys()) == 0:
+            f.close()
+            f = h5py.File(gcPath(basePath, snapNum, i), 'r')
+            i += 1
+
         # if fields not specified, load everything
         if not fields:
             fields = list(f[gName].keys())
@@ -66,30 +107,54 @@ def loadObjects(basePath, snapNum, gName, nName, fields):
             result[field] = np.zeros(shape, dtype=f[gName][field].dtype)
 
     # loop over chunks
-    wOffset = 0
+    if mp.current_process().name != "MainProcess":
+        nThreads = 1 # already inside daemonic child process, cannot spawn more children
+        
+    if nThreads == 1 or header['NumFiles'] == 1:
+        # serial load
+        wOffset = 0
 
-    for i in range(header['NumFiles']):
-        f = h5py.File(gcPath(basePath, snapNum, i), 'r')
+        for i in range(header['NumFiles']):
+            f = h5py.File(gcPath(basePath, snapNum, i), 'r')
 
-        if not f['Header'].attrs['N'+nName+'_ThisFile']:
-            continue  # empty file chunk
+            if not f['Header'].attrs['N'+nName+'_ThisFile']:
+                f.close()
+                continue  # empty file chunk
 
-        # loop over each requested field
-        for field in fields:
-            if field not in f[gName].keys():
-                raise Exception("Group catalog does not have requested field [" + field + "]!")
+            # loop over each requested field
+            for field in fields:
+                if field not in f[gName].keys():
+                    raise Exception("Group catalog does not have requested field [" + field + "]!")
 
-            # shape and type
-            shape = f[gName][field].shape
+                # shape and type
+                shape = f[gName][field].shape
 
-            # read data local to the current file
-            if len(shape) == 1:
-                result[field][wOffset:wOffset+shape[0]] = f[gName][field][0:shape[0]]
-            else:
-                result[field][wOffset:wOffset+shape[0], :] = f[gName][field][0:shape[0], :]
+                # read data local to the current file
+                result[field][wOffset:wOffset+shape[0], ...] = f[gName][field][()]
 
-        wOffset += shape[0]
-        f.close()
+            wOffset += shape[0]
+            f.close()
+    else:
+        # parallelized load
+        pool = mp.Pool(processes=nThreads)
+
+        fileNums = range(header['NumFiles'])
+        func = partial(_readfunc,basePath,snapNum,gName,nName,fields)
+        p_results = pool.map(func, fileNums)
+        pool.close()
+
+        # write
+        wOffset = 0
+
+        for i in range(header['NumFiles']):
+            if p_results[i] is None:
+                continue # no objects in this chunk
+
+            for field in fields:
+                numLoc = p_results[i][field].shape[0]
+                result[field][wOffset:wOffset+numLoc,...] = p_results[i][field]
+
+            wOffset += numLoc
 
     # only a single field? then return the array instead of a single item dict
     if len(fields) == 1:
